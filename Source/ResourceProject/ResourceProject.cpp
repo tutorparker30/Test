b4ba9f8a77242200174e548a14da39354a9024ac
PGH\qkrru <qkrrudgh30@naver.com>
2025-02-13 20:10:04 +0900

    add asset 2GB

---
+++ Source/ResourceProject/ResourceProject.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ResourceProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ResourceProject, "ResourceProject" );
